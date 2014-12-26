1a1d4ca7313350af5a00c69e822212252ecb236c
Ian Clark <idclark@Ians-MacBook.local>
2014-12-25 21:15:43 -0500

    finish view for stories

--- src/hn_clj/core/views/index.clj
+++ src/hn_clj/core/views/index.clj
@@ -1,3 +1,17 @@
 (ns hn-clj.views.stories.index
   (:require [hn-clj.views.layout :refer [main-layout]]
             [hn-clj.views.utilities :as util]))
+
+(defn story-html
+  [story]
+  [:li {:class "story"}
+   (util/link (story "title") (story "url"))
+   [:p
+    (story "score") " points by "
+    (util/user-link (story "by") (story "by")) " | "
+    (util/story-link
+     (str (count (story "kids")) " comments") (story "id"))]])
+(defn page
+  [stories]
+  (main-layout {:title "Hacker News Clone"}
+               [:ol (map story-html stories)]))
