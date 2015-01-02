3d6db30741c812919c59e337a61718834dfd9589
idclark <idclark13@gmail.com>
2015-01-02 09:51:12 -0500

    Get everything into a good state

--- project.clj
+++ project.clj
@@ -1,17 +1,15 @@
-(defproject hn-clj "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject hn-clj "0.1.1"
+  :description "A HackerNews Clone using the Firebase API"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.3.1"]
                  [ring/ring-defaults "0.1.2"]
-                 [ring/ring-jetty-adapter "1.3.1"]
                  [clj-http-lite "0.2.0"]
                  [cheshire "5.4.0"]
                  [hiccup "1.0.5"]]
   :plugins [[lein-ring "0.8.13"]]
   :ring {:handler hn-clj.core.handler/app}
-  :main hn-clj.core.handler
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring-mock "0.1.5"]]}})
--- src/hn_clj/core/handler.clj
+++ src/hn_clj/core/handler.clj
@@ -2,9 +2,7 @@
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [compojure.handler :as handler]
-            [hiccup.middleware :refer [wrap-base-url]]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
-            [ring.adapter.jetty :as jetty]
             [hn-clj.core.controllers.story :as story]
             [hn-clj.core.controllers.users :as users]
             ))
@@ -14,11 +12,5 @@
   (GET "/stories/:id" [id] (story/show-story id))
   (GET "/users/:username" [username] (users/show username)))
 
-(defn app
-  []
-  (-> (handler/site app-routes)
-      (wrap-base-url)))
-
-(defn -main
-  []
-  (jetty/run-jetty (app) {:port 3000}))
+(def app
+  (wrap-defaults app-routes site-defaults))
