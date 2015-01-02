6208804be84d8024709027f3a65abc8369ce3bd3
idclark <idclark13@gmail.com>
2015-01-02 14:36:51 -0500

    CSS to make it pretty

--- src/hn_clj/core/views/stories/index.clj
+++ src/hn_clj/core/views/stories/index.clj
@@ -7,7 +7,7 @@
   [:li {:class "story"}
    (util/link (story :title) (story :url))
    [:p
-    (story "score") " points by "
+    (story :score) " points by "
     (util/user-link (story :by) (story :by)) " | "
     (util/story-link
      (str (count (story :kids)) " comments") (story :id))]])
@@ -15,4 +15,4 @@
 (defn page
   [stories]
   (main-layout {:title "Hacker News Clone"}
-               [:ol (map story-html stories)]))
+               [:ol {:class "story-list"} (map story-html stories)]))
